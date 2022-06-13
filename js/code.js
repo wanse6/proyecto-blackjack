72885404d754b625c0e7540a0ebb9cd58b015284
wanse6 <tolarovicjuanse@gmail.com>
2022-06-13 05:17:15 -0300

    first upload - structuring

---
+++ js/code.js
@@ -0,0 +1,24 @@
+// INITIALIZING DISPLAYs FUNCTIONS
+function displayNone(id) {
+    document.getElementById(id).style.display = "none";
+  }
+  
+  function displayBlock(id) {
+    document.getElementById(id).style.display = "block";
+  }
+  
+  function displayInline(id) {
+    document.getElementById(id).style.display = "inline";
+  }
+  
+  function displayInlineBlock(id) {
+    document.getElementById(id).style.display = "inline-block";
+  }
+  
+  function displayFlex(id) {
+    document.getElementById(id).style.display = "flex";
+  }
+  
+  function displayInherit(id) {
+    document.getElementById(id).style.display = "inherit";
+  }  

93c94e939e8c6d3f5cd154c99ff37ad82adcdd59
wanse6 <tolarovicjuanse@gmail.com>
2022-06-14 05:28:30 -0300

    second commit - making the game

--- js/code.js
+++ js/code.js
@@ -1,24 +1,56 @@
-// INITIALIZING DISPLAYs FUNCTIONS
+// INITIALIZING DISPLAY FUNCTIONS
 function displayNone(id) {
-    document.getElementById(id).style.display = "none";
+  document.getElementById(id).style.display = "none";
+}
+
+function displayBlock(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function displayInline(id) {
+  document.getElementById(id).style.display = "inline";
+}
+
+function displayInlineBlock(id) {
+  document.getElementById(id).style.display = "inline-block";
+}
+
+function displayFlex(id) {
+  document.getElementById(id).style.display = "flex";
+}
+
+function displayInherit(id) {
+  document.getElementById(id).style.display = "inherit";
+}
+
+// INITIALIZING BACKGROUND-COLOR FUNCTIONS
+function backgroundColor(id, color) {
+  document.getElementById(id).style.backgroundColor = color;
+}
+
+// COUNTER SETINTERVAL
+var playPressed = false;
+var seconds = 0;
+
+
+function incrementSeconds() {
+    seconds += 1;
+    console.log(seconds);
+    if(seconds == 3){
+      displayBlock('game-stage');
+      displayNone('loading-stage');
+    }
+}
+
+function isPlayPressed() {
+  playPressed = true;
+  if(playPressed == true){
+    displayBlock('loading-stage');
+    var cancel = setInterval(incrementSeconds, 1000); 
   }
-  
-  function displayBlock(id) {
-    document.getElementById(id).style.display = "block";
-  }
-  
-  function displayInline(id) {
-    document.getElementById(id).style.display = "inline";
-  }
-  
-  function displayInlineBlock(id) {
-    document.getElementById(id).style.display = "inline-block";
-  }
-  
-  function displayFlex(id) {
-    document.getElementById(id).style.display = "flex";
-  }
-  
-  function displayInherit(id) {
-    document.getElementById(id).style.display = "inherit";
-  }  
+}
+
+
+
+
+
